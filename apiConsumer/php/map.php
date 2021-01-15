e373dcfb38322eb758c922ba87760e603f8ed02d
Muhitdin Yahya <monkeydjack0@mail.ru>
2021-01-15 13:39:16 +0100

    implemented delete function for json page

--- apiConsumer/php/map.php
+++ apiConsumer/php/map.php
@@ -7,6 +7,23 @@
 	<title>Map visualisation</title>
 </head>
 <body>
+    <header class="header">
+        <div class="container">
+            <div class="header-wrapper  clearfix">
+                <div class="header_logo">
+                    <h1 class="logo--smtxt"><span class="logo--lgtxt">Countries </span>Overview</h1>
+                </div>
+                <div class="header_navigator">
+                    <ul class="header_menu">
+                        <li class="header_menu_li"><a href="../index.php" class="menu_li_links">Index</a></li>
+                        <li class="header_menu_li"><a href="php/map.php" class="menu_li_links">Json routes</a></li>
+                        <li class="header_menu_li"><a href="#" class="menu_li_links">Xml routes</a></li>
+                        <li class="header_menu_li"><a href="#" class="menu_li_links">Maps</a></li>
+                    </ul>
+                </div>
+            </div>  
+        </div>
+    </header>
 	 <?php
 	 	$population = curl_init();
         $url = 'http://127.0.0.1:1234/population_info';
@@ -16,7 +33,7 @@
         curl_setopt($population,CURLOPT_RETURNTRANSFER,true);
         $information = curl_exec($population);
         curl_close($population);
-        $informationRecived = (array) json_decode($information);
+        $informationReceived = (array) json_decode($information);
         ?>
 
         <h1>Country population</h1>
@@ -25,25 +42,56 @@
             <div class="table-header">
                 <div class="header__item"><a id="country" class="filter__link" href="#">Country</a></div>
                 <div class="header__item"><a id="population" class="filter__link filter__link--number" href="#">Population </a></div>
-                <div class="header__item"><a id="y_change" class="filter__link filter__link--number" href="#">Yearly change</a></div>
-                <div class="header__item"><a id="l_area" class="filter__link filter__link--number" href="#">Land area</a></div>
+                <div class="header__item"><a id="y_change" class="filter__link filter__link--number" href="#">Yearly change(%)</a></div>
+                <div class="header__item"><a id="l_area" class="filter__link filter__link--number" href="#">Land area(Km²)</a></div>
                 <div class="header__item"><a id="migrants" class="filter__link filter__link--number" href="#">Migrants</a></div>
                 <div class="header__item"><a id="age" class="filter__link filter__link--number" href="#">Medium age</a></div>
+                <div class="header__item"><a class="filter__link filter__link--number" href="#">Actions</a></div>
             </div>
                 <div class="table-content"> 
                <?php
-        foreach ($informationRecived as $informationRecived)  
+
+               if(isset($_POST['delete'])) {
+                        if ($_POST['delete'] == 'Delete') {
+                            // edit the post with $_POST['id']
+                            $country =str_replace(' ', '',$_POST['country_delete']);
+                            $url = 'http://127.0.0.1:1234/population_info';
+                            $request_url = $url . '/' . $country;
+
+                            $delete = curl_init($request_url);
+                            curl_setopt($delete,CURLOPT_RETURNTRANSFER,true);
+                            curl_setopt($delete,CURLOPT_URL,$request_url);
+                            curl_setopt($delete, CURLOPT_CUSTOMREQUEST, 'DELETE');
+                            curl_setopt($delete,CURLOPT_HEADER, false);
+                            
+                            $response = curl_exec($delete);
+                            curl_close($delete);
+                            
+                            echo $response . PHP_EOL;
+                        }
+                    }
+
+
+                if(isset(var))
+        foreach ($informationReceived as $informationReceived)  
                 {
                     //insert data from json
                     echo '<div class="table-row">';
-                    echo '<div class="table-data">'.$informationRecived->country.'</div>';
-                    echo '<div class="table-data">'.$informationRecived->population_value.'</div>';
-                    echo '<div class="table-data">'.$informationRecived->yearly_change.'%</div>';        
-                    echo '<div class="table-data">'.$informationRecived->land_area.'Km²</div>'; 
-                    echo '<div class="table-data">'.$informationRecived->migrants.'</div>'; 
-                    echo '<div class="table-data">'.$informationRecived->med_age.'</div>';  
+                    echo '<div class="table-data">'.$informationReceived->country.'</div>';
+                    echo '<div class="table-data">'.$informationReceived->population_value.'</div>';
+                    echo '<div class="table-data">'.$informationReceived->yearly_change.'</div>';        
+                    echo '<div class="table-data">'.$informationReceived->land_area.'</div>'; 
+                    echo '<div class="table-data">'.$informationReceived->migrants.'</div>'; 
+                    echo '<div class="table-data">'.$informationReceived->med_age.'</div>';
+                    
+                    echo '<div class="table-data"><form action="" method="POST"><input type="submit" value="Edit">
+                   <input type="hidden" name="country_delete" value="'.$informationReceived->country.'"/><input type="submit" name="delete" value="Delete"></form> </div>';
                     echo '</div>';
                 }
+
+                    
+                    
+                
 	 ?>
 	 </div>
     </div>
--- settings.py
+++ settings.py
@@ -4,4 +4,4 @@ from flask_marshmallow import Marshmallow
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///country_overview.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
